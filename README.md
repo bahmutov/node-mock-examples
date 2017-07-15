239bdce787e1ffc7bbe7ae8db6a943e5087aa76d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-14 22:39:47 -0400

    add mock file system spec example

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ than stubbing your own internal code. And simpler to update in the future!
 * [x] [spec](test/fs-sandbox-spec.js) restoring all mocked methods via [sinon sandbox][sinon sandbox]
 * [x] [spec](test/console-spec.js) console mocking with [mock-stdio](https://github.com/catdad/mock-stdio)
 * [x] [spec](test/fs-callback-spec.js) async `fs` methods
-* [ ] entire file system with [mock-fs][mock-fs]
+* [x] [spec](test/fake-file-system-spec.js) entire file system with [mock-fs][mock-fs]
 * [ ] network request with [nock][nock]
 * [ ] `child_process.spawn`
 * [ ] `child_procesa.exec`
--- test/fs-callback-spec.js
+++ test/fs-callback-spec.js
@@ -1,31 +1,33 @@
-const fs = require('fs')
+const fse = require('fs-extra')
 
-function read(filename, cb) {
-  fs.readFile(filename, 'utf8', cb)
+// read all files and return concatenated text
+function readFiles(...filenames) {
+  return Promise.all(filenames.map(name => fse.readFile(name, 'utf8')))
+    .then(contents => contents.join('\n===\n'))
 }
 
-describe('mocking fs methods with callbacks', () => {
-  const sinon = require('sinon')
+describe('mocks several files by create a file system', () => {
+  const mock = require('mock-fs')
 
   beforeEach(() => {
-    // because fs.readFile calls 3rd argument with (err, text)
-    // we use .callsArgWith with index 2,
-    // null error and fake file contents
-    sinon.stub(fs, 'readFile')
-      .withArgs('foo.txt', 'utf8')
-      .callsArgWith(2, null, 'fake text')
+    mock({
+      'foo/bar/a.txt': 'text from file foo/bar/a.txt',
+      'foo/b.txt': 'text from file foo/b.txt',
+      'baz.txt': `
+        last file with some
+        some text in file baz.txt
+      `
+    })
   })
 
   afterEach(() => {
-    // restore individual methods
-    fs.readFile.restore()
+    mock.restore()
   })
 
-  it('reads non-existent file', (done) => {
-    read('foo.txt', (err, text) => {
-      console.assert(!err, 'no error expected')
-      console.assert(text === 'fake text', text)
-      done()
-    })
+  it('several files', () => {
+    return readFiles('foo/bar/a.txt', 'foo/b.txt', 'baz.txt')
+      .then(text => {
+        console.assert(text.includes('foo/b.txt'))
+      })
   })
 })
